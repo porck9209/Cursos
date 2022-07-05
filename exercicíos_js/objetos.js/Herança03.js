5abda2ca3ff70139f09ec7660cda94786678d88d
Matheus Esteves <95330382+porck9209@users.noreply.github.com>
2022-07-05 15:38:17 -0400

    .

---
+++ exercicíos_js/objetos.js/Herança03.js
@@ -0,0 +1,13 @@
+const pai = {nome: "pedro", corCabelo: "preto",}
+
+const filha1 = Object.Create(pai)
+filha1.nome = "ana"
+console.log(filha1.corCabelo)
+
+const filha2 = Object.create(pai,{
+    nome: "bia", writable: false, enumerable:true,
+})
+
+console.log(filha2.nome)
+filha2.nome = "cleiton"
+console.log(`${filha2.nome} e tem cabelo da cor ${filha2.corCabelo}`)
