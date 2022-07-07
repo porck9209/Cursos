7a129b74d9b50400ce00bc4f6bb77899c89bc31c
Matheus Esteves <95330382+porck9209@users.noreply.github.com>
2022-07-07 15:15:01 -0400

    .

---
+++ exercicíos_js/objetos.js/Herança06.js
@@ -0,0 +1,21 @@
+function aula (nome, videoID) {
+    this.nome = nome
+    this.videoID = videoID
+}
+
+const aula1 = new aula("Bem vindo!!",123)
+const aula2 = new aula("Até breve!!",654)
+console.log(aula1,aula2)
+
+//simulando o new
+
+function novo (f, ...paramas) {
+    const obj = {}
+    obj.__proto__ = f.prototype
+    f.apply(obj, paramas)
+    return obj
+}
+
+const aula3 = novo(aula, "Bem vindo!!", 123)
+const aula4 = novo(aula, "Ate breve!!", 456)
+console.log(aula3, aula4)
