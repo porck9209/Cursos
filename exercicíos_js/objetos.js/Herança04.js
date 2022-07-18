d6bb896d20bceac8d28579c5960394dff183ab79
Matheus Esteves <95330382+porck9209@users.noreply.github.com>
2022-07-18 10:11:58 -0300

    .

--- exercicíos_js/objetos.js/Herança04.js
+++ exercicíos_js/objetos.js/Herança04.js
@@ -1,7 +1,26 @@
-function MeuObjeto () {}
+function MeuObjeto() {}
 console.log(MeuObjeto.prototype)
 
 const obj1 = new MeuObjeto
 const obj2 = new MeuObjeto
 console.log(obj1.__proto__ === obj2.__proto__)
 console.log(MeuObjeto.prototype === obj1.__proto__)
+
+MeuObjeto.prototype.nome = "anonimo"
+MeuObjeto.prototype.falar = function() {
+    console.log(`ola!, ,eu nome é ${this.nome}!`)
+}
+
+obj1.falar()
+
+obj2.nome = "Rafaela"
+obj2.falar()
+
+const obj3 = {}
+obj3.__proto__ = MeuObjeto.prototype
+obj3.nome = "obj3"
+obj3.falar()
+
+
+
+console.log((new MeuObjeto).__proto__ === MeuObjeto.prototype)
